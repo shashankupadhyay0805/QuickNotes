731ab3f34ec41f49b206f38fe1329576b32bdc7a
shashankupadhyay0805 <147583703+shashankupadhyay0805@users.noreply.github.com>
2025-12-09 08:42:06 +0530

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,8 +1,22 @@
+# from pydantic import BaseModel
+
+# class NoteBase(BaseModel):
+#     title: str
+#     content: str | None = None
+
+# class NoteCreate(NoteBase):
+#     pass
+
+# class NoteOut(NoteBase):
+#     id: int
+
+#     class Config:
+#         orm_mode = True
 from pydantic import BaseModel
 
 class NoteBase(BaseModel):
     title: str
-    content: str | None = None
+    content: str
 
 class NoteCreate(NoteBase):
     pass
@@ -12,3 +26,4 @@ class NoteOut(NoteBase):
 
     class Config:
         orm_mode = True
+
