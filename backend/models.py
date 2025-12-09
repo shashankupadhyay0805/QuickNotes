3798ec516aa2f68722381253377bd0d9d0670047
shashankupadhyay0805 <147583703+shashankupadhyay0805@users.noreply.github.com>
2025-12-09 08:41:09 +0530

    Refactor Note model fields and indexing
    Updated Note model to change content type and add index to title.

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,20 @@
-# from sqlalchemy import Column, Integer, String, Text
-# from .database import Base
-from backend.database import Base
+# # from sqlalchemy import Column, Integer, String, Text
+# # from .database import Base
+# from backend.database import Base
+# from sqlalchemy import Column, Integer, String
+
+# class Note(Base):
+#     __tablename__ = "notes"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String, nullable=False)
+#     content = Column(Text, nullable=True)
 from sqlalchemy import Column, Integer, String
+from database import Base
 
 class Note(Base):
     __tablename__ = "notes"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    content = Column(Text, nullable=True)
+    title = Column(String, index=True)
+    content = Column(String)
